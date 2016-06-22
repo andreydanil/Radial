b3041c9e210251309a0557d57e0a65d47bdb1c22
Andrey Danil <adanilko@hawk.iit.edu>
2016-06-21 21:02:56 -0500

    updating pods 3

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,62 +1,19 @@
 //: Playground - noun: a place where people can play
 
-import Cocoa
+import UIKit
 
 var str = "Hello, playground"
 
 
-// Simple REST API Calls with Swift
-
-let config = NSURLSessionConfiguration.defaultSessionConfiguration()
-let session = NSURLSession(configuration: config)
-let task = session.dataTaskWithRequest(urlRequest, completionHandler: nil)
-
-
-let postEndpoint: String = "http://jsonplaceholder.typicode.com/posts/1"
-guard let url = NSURL(string: postEndpoint) else {
-    print("Error: cannot create URL")
+class Cars <T>
+{
+    var model : T
+    var make : T
+    var year: T
 }
-let urlRequest = NSURLRequest(URL: url)
 
 
+var myArray : [String];()
+var myDictionary : [String : String];()
+var mySet : Set<String> = ["object”, ”object2”]
 
-// REST API Calls with Alamofire & SwiftyJSON
-
-let postEndpoint: String = "http://jsonplaceholder.typicode.com/posts/1"
-guard let url = NSURL(string: postEndpoint) else {
-    print("Error: cannot create URL")
-    return
-}
-let urlRequest = NSURLRequest(URL: url)
-let config = NSURLSessionConfiguration.defaultSessionConfiguration()
-let session = NSURLSession(configuration: config)
-let task = session.dataTaskWithRequest(urlRequest, completionHandler: { (data, response, e\
-    rror) in
-    guard let responseData = data else {
-        print("Error: did not receive data")
-        return
-    }
-    guard error == nil else {
-        print("error calling GET on /posts/1")
-        print(error)
-        return
-    }
-    // parse the result as JSON, since that's what the API provides
-    let post: NSDictionary
-    do {
-        post = try NSJSONSerialization.JSONObjectWithData(responseData,
-            options: []) as! NSDictionary
-    } catch {
-        print("error trying to convert data to JSON")
-        return
-    }
-    // now we have the post, let's just print it to prove we can access it
-    print("The post is: " + post.description)
-    // the post object is a dictionary
-    // so we just access the title using the "title" key
-    // so check for a title and print it if we have one
-    if let postTitle = post["title"] as? String {
-        print("The title is: " + postTitle)
-    }
-})
-task.resume()
